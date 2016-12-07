445cc13a1841c86485c55b7c874cf7bb7285bb33
Semion Akimtsev <s.akimtsev@sdventures.com>
2016-12-07 15:15:03 +0300

    NSpec based unit tests project added; paket references split in groups to support multiple specific nunit versions

---
+++ Tests/Contour.Tests/DebuggerShim.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using NSpec;
+using NSpec.Domain;
+using NSpec.Domain.Formatters;
+using NUnit.Framework;
+
+namespace Contour.Tests
+{
+    [TestFixture]
+    [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Reviewed. Suppression is OK here.")]
+    // ReSharper disable once InconsistentNaming
+    public abstract class NSpec : global::NSpec.nspec
+    {
+        [Test]
+        // ReSharper disable once InconsistentNaming
+        public void debug()
+        {
+            var currentSpec = this.GetType();
+            var finder = new SpecFinder(new[] { currentSpec });
+            var tagsFilter = new Tags().Parse(currentSpec.Name);
+
+            var builder = new ContextBuilder(finder, tagsFilter, new DefaultConventions());
+            var runner = new ContextRunner(tagsFilter, new ConsoleFormatter(), false);
+            var results = runner.Run(builder.Contexts().Build());
+
+            results.Failures().Count().should_be(0);
+        }
+    }
+}
