181128acfe49462f2762d9a20f24e28f6fe091f4
a.prokopyev <a.prokopyev@it-online.ru>
2017-03-07 11:46:36 +0300

    added extension properties for cache configuration element

--- Sources/Contour/Configurator/CachingElement.cs
+++ Sources/Contour/Configurator/CachingElement.cs
@@ -1,5 +1,6 @@
 ﻿namespace Contour.Configurator
 {
+    using System.Collections.Generic;
     using System.Configuration;
 
     /// <summary>
@@ -17,6 +18,25 @@
             {
                 return (bool)(base["enabled"]);
             }
+        }
+
+        /// <summary>
+        /// Configuration extension properties
+        /// </summary>
+        public IDictionary<string, string> ExtensionProperties { get; } = new Dictionary<string, string>();
+
+        /// <summary>
+        /// Gets a value indicating whether an unknown attribute is encountered during deserialization
+        /// </summary>
+        /// <returns>
+        /// true when an unknown attribute is encountered while deserializing; otherwise, false
+        /// </returns>
+        /// <param name="name">The name of the unrecognized attribute</param>
+        /// <param name="value">The value of the unrecognized attribute</param>
+        protected override bool OnDeserializeUnrecognizedAttribute(string name, string value)
+        {
+            this.ExtensionProperties[name] = value;
+            return true;
         }
     }
 }
