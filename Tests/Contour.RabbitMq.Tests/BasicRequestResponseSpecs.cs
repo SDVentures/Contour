4869dccd08e9b00df6f43153f540a36a5aa1bc05
Semion Akimtsev <s.akimtsev@sdventures.com>
2017-03-22 18:42:46 +0300

    Namespace references fixed

--- Tests/Contour.RabbitMq.Tests/BasicRequestResponseSpecs.cs
+++ Tests/Contour.RabbitMq.Tests/BasicRequestResponseSpecs.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-
+using System.Dynamic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
