c1df0f19cf609a514055fa8c9a0671f0bea3b124
a.prokopyev <a.prokopyev@it-online.ru>
2017-06-09 17:14:10 +0300

    fixed code style

--- Sources/Contour/Filters/IMessageExchangeFilterDecorator.cs
+++ Sources/Contour/Filters/IMessageExchangeFilterDecorator.cs
@@ -1,7 +1,7 @@
-﻿namespace Contour.Filters
-{
-    using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
+namespace Contour.Filters
+{
     public interface IMessageExchangeFilterDecorator
     {
         Task<MessageExchange> Process(IMessageExchangeFilter filter, MessageExchange exchange, MessageExchangeFilterInvoker invoker);
