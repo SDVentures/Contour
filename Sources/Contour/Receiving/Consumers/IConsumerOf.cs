f684b11feb019df286633d4c3ff17566550a6d4e
asenokosovSDV <52970041+asenokosovSDV@users.noreply.github.com>
2021-06-03 12:32:05 +0300

    Костыли для асинхронного потребления сообщений (#114)
    Co-authored-by: BlackBred <black.bred@gmail.com>

--- Sources/Contour/Receiving/Consumers/IConsumerOf.cs
+++ Sources/Contour/Receiving/Consumers/IConsumerOf.cs
@@ -7,6 +7,9 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System;
+using System.Threading.Tasks;
+
 namespace Contour.Receiving.Consumers
 {
     /// <summary>
@@ -25,7 +28,20 @@ namespace Contour.Receiving.Consumers
         /// <param name="context">
         /// The context.
         /// </param>
+        [Obsolete]
         void Handle(IConsumingContext<T> context);
+
+        #endregion
+    }
+
+
+    //todo отделить от IConsumerOf<T>
+    public interface IAsyncConsumerOf<T> : IConsumerOf<T>
+      where T : class
+    {
+        #region Public Methods and Operators
+
+        Task HandleAsync(IConsumingContext<T> context);
 
         #endregion
     }
